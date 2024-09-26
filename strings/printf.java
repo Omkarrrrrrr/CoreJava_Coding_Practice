72af82b9febfc37c0f4367edc88bf199dbe854aa
Omkarrrrrrr <ominalawade77@gmail.com>
2024-09-26 11:25:53 +0530

    printf

--- strings/printf.java
+++ strings/printf.java
@@ -5,9 +5,21 @@ public class printf {
     public static void main(String[] args) {
         String name = "Alice";
         int score = 95;
-        
+
         // Using printf for formatted output
-        System.out.printf("Student: %s, Score: %d\n", name, score);  // Output: Student: Alice, Score: 95 
+        System.out.printf("Student: %s, Score: %d\n", name, score); // Output: Student: Alice, Score: 95
+
+        /*
+         * format():
+         * Returns the formatted string.
+         * Typically used when you need to store or further manipulate the formatted
+         * string.
+         * 
+         * 
+         * printf():
+         * Prints the formatted string directly to the console.
+         * Used when you want to output formatted text immediately.
+         */
     }
 
 }
